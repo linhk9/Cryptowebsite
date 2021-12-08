c483f24bb5bcf7bb5b4bdb5c3f2ca6e1428f4bc7
Rodrigo Luís <54328638+linhk9@users.noreply.github.com>
2021-12-08 01:12:11 +0000

    Começo basico do website

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const cryptoApi = "https://api.coingecko.com/api/v3";
+
+const moeda = "eur";
+const limiteLista = 100;
+
+$.getJSON(cryptoApi+"/coins/markets?vs_currency="+moeda+"&order=market_cap_desc&per_page="+limiteLista+"&page=1&sparkline=false", function(data){
+    data.forEach(cur => {
+        console.log(cur);
+    });
+}).fail(function(dat, textStatus, error ) {
+    console.log("API: /coins/list > falhou")
+});
---
+++ js/themeswitcher.js
@@ -0,0 +1,47 @@
+// you can use app's unique identifier here
+const LOCAL_STORAGE_KEY = "toggle-bootstrap-theme";
+
+const LOCAL_META_DATA = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+
+// you can change this url as needed
+const DARK_THEME_PATH = "./css/bootstrap-dark.min.css";
+
+const DARK_STYLE_LINK = document.getElementById("dark-theme-style");
+const THEME_TOGGLER = document.getElementById("theme-toggler");
+const THEME_TOGGLER_INPUT = document.getElementById("theme-toggler-input");
+
+let isDark = LOCAL_META_DATA && LOCAL_META_DATA.isDark;
+
+// check if user has already selected dark theme earlier
+if (isDark) {
+  enableDarkTheme();
+} else {
+  disableDarkTheme();
+}
+
+/**
+ * Apart from toggling themes, this will also store user's theme preference in local storage.
+ * So when user visits next time, we can load the same theme.
+ *
+ */
+function toggleTheme() {
+  isDark = !isDark;
+  if (isDark) {
+    enableDarkTheme();
+  } else {
+    disableDarkTheme();
+  }
+  localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify({ isDark }));
+}
+
+function enableDarkTheme() {
+  DARK_STYLE_LINK.setAttribute("href", DARK_THEME_PATH);
+  THEME_TOGGLER.innerHTML = "🌙";
+  THEME_TOGGLER_INPUT.checked = true;
+}
+
+function disableDarkTheme() {
+  DARK_STYLE_LINK.setAttribute("href", "");
+  THEME_TOGGLER.innerHTML = "🌞";
+  THEME_TOGGLER_INPUT.checked = false;
+}
