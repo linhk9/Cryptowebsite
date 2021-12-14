c232a84fba4831ac71d1e51977003ce2b078f186
Rodrigo Luís <54328638+linhk9@users.noreply.github.com>
2021-12-14 13:54:25 +0000

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const cryptoApi = "https://api.coingecko.com/api/v3";
 
 const moeda = "eur";
-const limiteLista = 10;
+const limiteLista = 100;
 
 $.getJSON(cryptoApi+"/coins/markets?vs_currency="+moeda+"&order=market_cap_desc&per_page="+limiteLista+"&page=1&sparkline=false", function(data){
     data.forEach(cur => {
